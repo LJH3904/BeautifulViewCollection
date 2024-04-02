bafc39c6fd1f2c063d07cea27f6aff951fa88631
Jehyeon Lee <dasom8899981@gmail.com>
2024-04-02 20:25:57 +0900

    docs: Assest 앱 이미지 추가

---
+++ Africa/Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by LJh on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Africa/Africa/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Africa
+//
+//  Created by LJh on 4/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
