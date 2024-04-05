291ec44d173b596fbd85a56ccf0c684bc8e8b8fa
Jehyeon Lee <dasom8899981@gmail.com>
2024-04-06 04:05:20 +0900

    Update CodableBundleExtension.swift

--- Africa/Africa/Extension/CodableBundleExtension.swift
+++ Africa/Africa/Extension/CodableBundleExtension.swift
@@ -12,12 +12,12 @@ extension Bundle {
         
         // 1. Locate the Json file
         guard let url = self.url(forResource: file, withExtension: nil) else {
-            fatalError("Failed to lacate \(file) in bundle ")
+            fatalError("Failed to locate \(file) in bundle ")
         }
         
         // 2. create a property for the data
         guard let data = try? Data(contentsOf: url) else {
-            fatalError("Failed to lacate \(file) from bundle")
+            fatalError("Failed to load \(file) from bundle")
         }
         
         // 3. create a decoder
@@ -25,12 +25,10 @@ extension Bundle {
         
         // 4. create a property for the decoded data
         guard let loaded = try? decoder.decode([CoverImage].self, from: data) else {
-            fatalError("Failed to lacate \(file) from bundle")
+            fatalError("Failed to decode \(file) from bundle")
 
         }
         // 5,  return the ready to use data
-        
-        
         return loaded
     }
 }
