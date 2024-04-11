b472c4888d9de04563307829170aa64bee20c8bf
Jehyeon Lee <dasom8899981@gmail.com>
2024-04-12 00:12:07 +0900

    create: VideoModel file

---
+++ Africa/Africa/Model/VideoModel.swift
@@ -0,0 +1,14 @@
+//
+//  VideoModel.swift
+//  Africa
+//
+//  Created by LJh on 4/12/24.
+//
+
+import Foundation
+
+struct Video: Codable, Identifiable {
+    let id: String
+    let name: String
+    let headline: String
+}
