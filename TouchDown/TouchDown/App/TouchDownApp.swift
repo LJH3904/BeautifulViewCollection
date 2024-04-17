97aeeceab9ae2f32c167642906620bd996569d70
Jehyeon Lee <dasom8899981@gmail.com>
2024-04-17 23:09:58 +0900

    create: TouchDown App feat: 파일구조 형성

---
+++ TouchDown/TouchDown/App/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by LJh on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TouchDown/TouchDown/Utility/Constant.swift
@@ -0,0 +1,29 @@
+//
+//  Constant.swift
+//  TouchDown
+//
+//  Created by LJh on 4/17/24.
+//
+
+import SwiftUI
+
+// Data
+
+// color
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
+// Layout
+
+// UX
+
+// API
+
+// Image
+
+// FOnt
+
+// String
+
+// Misc
