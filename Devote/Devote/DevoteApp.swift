f9f8b55d1ce0f53b5b7428c11d8bde4fbce54868
Jehyeon Lee <dasom8899981@gmail.com>
2024-04-23 21:23:05 +0900

    create: Devote 프로젝트 생성

---
+++ Devote/Devote/DevoteApp.swift
@@ -0,0 +1,20 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by LJh on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
